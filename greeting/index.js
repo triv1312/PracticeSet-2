fb0d3e429df87719527662990491a85edae1ec52
TriveniThelaprolu <trivenitelaprolu@gmail.com>
2022-12-01 17:12:20 +0000

    Greeting Message

---
+++ greeting/index.js
@@ -0,0 +1,2 @@
+
+module.exports = "Have a Great Day";
---
+++ message/index.js
@@ -0,0 +1,8 @@
+const a = require("greeting/index.js");
+const string = "Hello Rahul!";
+
+module.exports = string;
+
+module.exports = string + a;
+
+console.log(string);
